b14126d5c98a1e464405320ad4e233d7b48d2032
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-05-06 15:04:03 -0300

    feat: cria models

--- models/carrinho.js
+++ models/carrinho.js
@@ -1,4 +1,10 @@
-const produtoSchema = new mongoose.Schema({
-    nome: String,
-    preco: Number
-})
+const { default: mongoose } = require("mongoose");
+
+const carrinhoSchema = new mongoose.Schema({
+    nome: String, 
+    preco: Number,
+    quantidade: Number
+
+});
+
+module.exports = mongoose.model("Carrinho", carrinhoSchema)
